6d7465ed9f850fbd3c78e544ad21f370f4a513e8
Achintha Rodrigo <achintha@riverviewms.com>
2015-01-13 21:59:07 +0530

    initial changes

---
+++ src/SimpleSoftwareIO/SMS/Drivers/ClickatellSMS.php
@@ -0,0 +1,16 @@
+<?php namespace SimpleSoftwareIO\SMS\Drivers;
+
+/**
+ * Simple-SMS
+ * A simple SMS message sending for Laravel.
+ *
+ * @link http://www.simplesoftware.io
+ * @author SimpleSoftware support@simplesoftware.io
+ *
+ */
+
+use SimpleSoftwareIO\SMS\OutgoingMessage;
+
+class ClickatellSMS extends AbstractSMS implements DriverInterface {
+	
+}
